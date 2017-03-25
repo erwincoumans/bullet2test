ddc0d1360871be411fbe519361ce68377c81e08f
Erwin Coumans <erwincoumans@google.com>
2017-03-25 08:38:21 -0700

    fix style in main.cpp

--- main.cpp
+++ main.cpp
@@ -4,11 +4,11 @@
 
 int main(int argc, char* argv[])
 {
-btVector3 a(0,0,0);
-btVector3 b(1,1,1);
-btVector3 c;
+	btVector3 a(0, 0, 0);
+	btVector3 b(1, 1, 1);
+	btVector3 c;
 
-c = a+b;
-printf("c=%f,%f,%f\n", c[0],c[1],c[2]);
-printf("hello\n");
+	c = a + b;
+	printf("c=%f,%f,%f\n", c[0], c[1], c[2]);
+	printf("hello\n");
 }
